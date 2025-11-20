b3f5446f071c516bda890c1e6600736ae48403c3
Tw212 <Tewe07001@utb.vaxjo.se>
2025-11-20 10:04:30 +0100

    glömde

---
+++ Basklass.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Spel_project_ht25
+{
+    public abstract class Basklass
+    {
+        protected Color color;
+        protected Vector2 postion;
+        protected Rectangle hitbox;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new Spel_project_ht25.Game1();
+game.Run();
